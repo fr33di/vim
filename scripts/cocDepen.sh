3339a1bf8b5d1ca9641625019b36e1f0a2da6f0a
fr33di <168632239+fr33di@users.noreply.github.com>
2025-03-28 16:43:47 -0400

    Update cocDepen.sh

--- scripts/cocDepen.sh
+++ scripts/cocDepen.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Ejecutar Vim con el comando CocInstall para instalar las extensiones
+# Ejecutar Vim para instalar los plugins con vim-plug
+echo "Instalando plugins con vim-plug..."
+vim +PlugInstall +qall
+
+# Instalar las extensiones de CoC
+echo "Instalando las extensiones de CoC..."
 vim +CocInstall\ 'coc-tsserver' 'coc-html' 'coc-css' 'coc-json' 'coc-emmet' 'coc-python' +qall
+
+echo "Instalación completada."
